d477f5ff70bbac057215a26faf48fed1d420bee3
Kurizu <83665497+crizmo@users.noreply.github.com>
2024-12-31 13:35:37 +0530

    yup

--- tests/core.js
+++ tests/core.js
@@ -1,42 +1,42 @@
-import * as japaneseeasy from '../index.js';
+import * as japanese_easy from '../index.js';
 
 // Fetch Kanji data
-japaneseeasy.lookupKanji('猫').then(data => {
+japanese_easy.lookupKanji('猫').then(data => {
     console.log("Result from lookupKanji:");
     console.log(data);
     console.log("End of lookupKanji result\n");
 });
 
 // Fetch Kana data
-japaneseeasy.lookupKana('あ').then(data => {
+japanese_easy.lookupKana('あ').then(data => {
     console.log("Result from lookupKana:");
     console.log(data);
     console.log("End of lookupKana result\n");
 });
 
 // Convert Kanji to Kana
-japaneseeasy.convertKanjiToKana('猫').then(data => {
+japanese_easy.convertKanjiToKana('猫').then(data => {
     console.log("Result from convertKanjiToKana:");
     console.log(data);
     console.log("End of convertKanjiToKana result\n");
 });
 
 // Fetch Kanji list
-japaneseeasy.fetchKanjiList('grade-1').then(data => {
+japanese_easy.fetchKanjiList('grade-1').then(data => {
     console.log("Result from fetchKanjiList:");
     console.log(data);
     console.log("End of fetchKanjiList result\n");
 });
 
 // Fetch Hiragana chart
-japaneseeasy.getHiraganaChart().then(data => {
+japanese_easy.getHiraganaChart().then(data => {
     console.log("Result from getHiraganaChart:");
     console.log(data);
     console.log("End of getHiraganaChart result\n");
 });
 
 // Fetch Katakana chart
-japaneseeasy.getKatakanaChart().then(data => {
+japanese_easy.getKatakanaChart().then(data => {
     console.log("Result from getKatakanaChart:");
     console.log(data);
     console.log("End of getKatakanaChart result\n");
